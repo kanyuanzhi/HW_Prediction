079cf873eda588b01111a008f848069949b98969
kanyuanzhi <kan@mail.ustc.edu.cn>
2018-03-23 01:09:00 +0800

    重新封装各函数

--- readtxt.py
+++ readtxt.py
@@ -1,19 +1,11 @@
 # *coding=utf8
-from datetime import date, timedelta
+from string_tools import str_to_date
 
 
-def str_to_date(str):
-    """将字符串转换为标准日期
-    example: '2015-02-20'->2015-02-20
-    :param str:
-    :return: Date格式日期
-    """
-    str_array = str.split('-')
-    return date(int(str_array[0]), int(str_array[1]), int(str_array[2]))
-
-
-# 处理input.txt文件
 class InputTxtProcess():
+    """
+    处理input.txt文件
+    """
     __f_input = open("input_5flavors_cpu_7days.txt")
     __input_data = __f_input.readlines()
     __f_input.close()
@@ -40,7 +32,10 @@ class InputTxtProcess():
         return self.__prediction_start_date
 
 
-class DataTrainTxtProcess():
+class TrainDataTxtProcess():
+    """
+    处理TrainData.txt文件
+    """
     __f_train = open("TrainData.txt")
     __train_data = __f_train.readlines()
     __f_train.close()
@@ -51,9 +46,17 @@ class DataTrainTxtProcess():
         __data_matrix.append(d.split('\t'))
 
     def start_date(self):
+        """
+        获取数据集的最早日期
+        :return:
+        """
         return str_to_date(self.__data_matrix[0][2].split(' ')[0])
 
     def flavor_name(self):
+        """
+        获取数据集中出现的flavor名称
+        :return:
+        """
         flavor_name_temp = []
         flavor_id = []
         for dm in self.__data_matrix:
@@ -68,7 +71,11 @@ class DataTrainTxtProcess():
         return self.__flavor_name
 
     def flavor_name_datetime(self, flavor_name):
-        # 提取flavor对应的所有时间点
+        """
+        获取各flavor对应的所有时间点
+        :param flavor_name:
+        :return:
+        """
         for fn in flavor_name:
             fnd_item = []
             for dm in self.__data_matrix:
--- test.py
+++ test.py
@@ -2,5 +2,3 @@ import readtxt as rd
 
 ss = rd.input_txt_process()
 print ss.get_delta()
-
-rd.

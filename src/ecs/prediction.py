be2aed321565d181bb196bdf130eeab0b7f9a390
kanyuanzhi <kan@mail.ustc.edu.cn>
2018-03-24 22:26:23 +0800

    original2.0

--- src/ecs/prediction.py
+++ src/ecs/prediction.py
@@ -17,10 +17,24 @@ def __average(prediction_numbers):
     return int(sum / len(prediction_numbers))
 
 
+def __onetime_exponential_smoothing(prediction_numbers):
+    alpha = 1.15
+    s1 = sum(prediction_numbers[0:3]) / 3.0
+    s_list = []
+    s_list.append(s1)
+    for i in range(1, len(prediction_numbers)):
+        s = alpha * prediction_numbers[i] + (1 - alpha) * s_list[i - 1]
+        s_list.append(s)
+
+    num = int(round(s_list[len(s_list) - 1]))
+    if num < 0:
+        num = 0
+    return num
+
+
 def prediction(ecs_lines, input_lines):
     period_data = data_process(ecs_lines, input_lines)
     flavor_prediction_numbers = []
-
     # example ######
     # for ps in period_data:
     #    flavor_prediction_numbers.append(__your_prediction(ps[1]))
@@ -28,8 +42,9 @@ def prediction(ecs_lines, input_lines):
 
     # test ######
     for ps in period_data:
-        flavor_prediction_numbers.append(__average(ps[1]))
-    #########################
+        # flavor_prediction_numbers.append(__average(ps[1]))
+        flavor_prediction_numbers.append(__onetime_exponential_smoothing(ps[1]))
+        #########################
         # print ps[2]
         # print ps[0]
         # print ps[1]
